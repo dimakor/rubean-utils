e8744f0873886dba4cefd1040dfd09051be2af29
dimakor <39400638+dimakor@users.noreply.github.com>
2023-05-28 11:27:35 +0300

    fetch ALL instruments
    including instruments removed from exchange

--- importers/tcsinvest/tcsdownload.py
+++ importers/tcsinvest/tcsdownload.py
@@ -1,7 +1,7 @@
 import os
 import configparser
 from urllib import response
-from tinkoff.invest import Client, PositionsResponse, MoneyValue, OperationState, OperationsResponse, Operation, AccessLevel
+from tinkoff.invest import Client, PositionsResponse, MoneyValue, OperationState, OperationsResponse, Operation, AccessLevel, OperationType, InstrumentStatus
 from rich import print
 from decimal import *
 import json
@@ -41,29 +41,44 @@ def get_positions_cash(response: PositionsResponse, tickers: dict) -> dict:
 
 def get_operations_list(resp: OperationsResponse, tickers: dict) -> list:
     result = list()
+    print("OPER: ", len(resp.operations))
     for op in resp.operations:
         # 1. find out ticker of asset
-        if op.figi == '':
-            # Cash operations
-            ticker = op.currency.upper()  # use operation's currency as ticker
-        elif tickers[op.figi]['type'] == 'currency':
-            # Currency conversion
-            ticker = tickers[op.figi]['nominal_currency']
-        else:
-            ticker = tickers[op.figi]['ticker']
+        try:
+            if op.figi == '':
+                # Cash operations
+                name = ticker = op.currency.upper()  # use operation's currency as ticker
+                asset_type = 'cash'
+            # elif op.figi == 'TCS007288411':
+            #     op.figi = 'BBG004731489'
+            #     ticker = 'GMKN'
+            # elif op.operation_type in [OperationType.OPERATION_TYPE_DIVIDEND_TAX, OperationType.OPERATION_TYPE_OVERNIGHT]:
+            #     continue
+            elif tickers[op.figi]['type'] == 'currency':
+                # Currency conversion
+                ticker = tickers[op.figi]['nominal_currency']
+                name = tickers[op.figi]['name']
+                asset_type = tickers[op.figi]['type']
+            else:
+                ticker = tickers[op.figi]['ticker']
+                name = tickers[op.figi]['name']
+                asset_type = tickers[op.figi]['type']
+        
+            oper_dict = {'date': str(op.date.date()),
+                        'id': op.id,
+                        'parent_id': op.parent_operation_id,
+                        'label' : op.type,
+                        'type': op.operation_type,
+                        'figi': op.figi,
+                        'name': name,
+                        'ticker': ticker,
+                        'asset type': asset_type,
+                        'payment': mv2str(op.payment), 
+                        'payment_currency' : op.currency.upper()
+                        }
+        except KeyError:
+            print(op)
 
-        oper_dict = {'date': str(op.date.date()),
-                     'id': op.id,
-                     'parent_id': op.parent_operation_id,
-                     'label' : op.type,
-                     'type': op.operation_type,
-                     'figi': op.figi,
-                     'name': op.currency.upper() if op.figi == '' else tickers[op.figi]['name'],
-                     'ticker': ticker,
-                     'asset type': 'cash' if op.figi == '' else tickers[op.figi]['type'],
-                     'payment': mv2str(op.payment), 
-                     'payment_currency' : op.currency.upper()
-                     }
         if len(op.trades) != 0:
             trades = list()
             for tr in op.trades:
@@ -89,12 +104,13 @@ token = configParser.get('tcsinvest', 'token')
 output = configParser.get('tcsinvest', 'output')
 
 result = {}
-client = Client(token)
+#client = Client(token)
 with Client(token) as client:
-    tickers = (get_tickers_list(client.instruments.shares().instruments, 'share') |
-               get_tickers_list(client.instruments.bonds().instruments, 'bond') |
-               get_tickers_list(client.instruments.etfs().instruments, 'etf') |
-               get_tickers_list(client.instruments.currencies().instruments, 'currency'))
+    #print(client.instruments.currencies(instrument_status = InstrumentStatus.INSTRUMENT_STATUS_ALL).instruments)
+    tickers = (get_tickers_list(client.instruments.shares(instrument_status = InstrumentStatus.INSTRUMENT_STATUS_ALL).instruments, 'share') |
+               get_tickers_list(client.instruments.bonds(instrument_status = InstrumentStatus.INSTRUMENT_STATUS_ALL).instruments, 'bond') |
+               get_tickers_list(client.instruments.etfs(instrument_status = InstrumentStatus.INSTRUMENT_STATUS_ALL).instruments, 'etf') |
+               get_tickers_list(client.instruments.currencies(instrument_status = InstrumentStatus.INSTRUMENT_STATUS_ALL).instruments, 'currency'))
     accounts = client.users.get_accounts().accounts
     for acc in accounts:
         if acc.access_level == AccessLevel.ACCOUNT_ACCESS_LEVEL_NO_ACCESS:
